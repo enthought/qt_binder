e38def47f77031b281c3661af31791dc4c69750c
Ioannis Tziakos <ioannist@enthought.com>
2015-10-12 23:56:12 +0100

    add a module to mock Qt wrappers during documentation build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,9 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 
+from mock_missing import mock_modules
+mock_modules()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
