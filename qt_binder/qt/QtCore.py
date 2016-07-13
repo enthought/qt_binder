e7d5cad7e51efa81c72b58594ba1e32c68ae600d
Robert Kern <robert.kern@gmail.com>
2016-07-13 16:13:59 +0100

    Merge pull request #49 from enthought/fix/flake8
    Fix ignorable flake errors

--- qt_binder/qt/QtCore.py
+++ qt_binder/qt/QtCore.py
@@ -1,20 +1,21 @@
+# flake8: noqa
 from . import qt_api
 
 if qt_api == 'pyqt':
-    from PyQt4.QtCore import *  # noqa
+    from PyQt4.QtCore import *
 
-    from PyQt4.QtCore import pyqtProperty as Property  # noqa
-    from PyQt4.QtCore import pyqtSignal as Signal  # noqa
-    from PyQt4.QtCore import pyqtSlot as Slot  # noqa
-    from PyQt4.Qt import QCoreApplication  # noqa
-    from PyQt4.Qt import Qt  # noqa
+    from PyQt4.QtCore import pyqtProperty as Property
+    from PyQt4.QtCore import pyqtSignal as Signal
+    from PyQt4.QtCore import pyqtSlot as Slot
+    from PyQt4.Qt import QCoreApplication
+    from PyQt4.Qt import Qt
 
     __version__ = QT_VERSION_STR
     __version_info__ = tuple(map(int, QT_VERSION_STR.split('.')))
 
 else:
     try:
-        from PySide import __version__, __version_info__  # noqa
+        from PySide import __version__, __version_info__
     except ImportError:
         pass
-    from PySide.QtCore import *  # noqa
+    from PySide.QtCore import *
