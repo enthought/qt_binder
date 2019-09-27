0c1886666d293e55488b4a22acf5dca627eb2618
Robert Kern <robert.kern@gmail.com>
2019-09-26 22:48:07 -0500

    ENH: Remove Python 2 logging nonsense.

--- qt_binder/__init__.py
+++ qt_binder/__init__.py
@@ -11,23 +11,3 @@
 #  Thanks for using Enthought open source!
 #
 #------------------------------------------------------------------------------
-
-# Add a NullHandler so loggers don't complain when they get used.
-import logging
-
-
-class NullHandler(logging.Handler):
-
-    def handle(self, record):
-        pass
-
-    def emit(self, record):
-        pass
-
-    def createLock(self):
-        self.lock = None
-
-logger = logging.getLogger(__name__)
-logger.addHandler(NullHandler())
-
-del logging, logger, NullHandler
