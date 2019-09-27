23a6bd96fdd119754f4d3bb548c78770f87730bc
Robert Kern <robert.kern@gmail.com>
2019-09-27 17:33:17 -0500

    ENH: Workaround upstream PySide2 bug that exists in 5.13.1 at least.

--- qt_binder/qt/__init__.py
+++ qt_binder/qt/__init__.py
@@ -1,1 +1,8 @@
 from pyface.qt import is_qt4, is_qt5, qt_api  # noqa
+
+if qt_api == 'pyside2':
+    # Work around https://bugreports.qt.io/browse/PYSIDE-1093
+    from pyface.qt.QtGui import QApplication
+
+    if QApplication.startingUp():
+        app = QApplication([])
