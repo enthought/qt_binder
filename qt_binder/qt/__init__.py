e23770bf94f45f61d40ebd40e482435e837c14ac
Robert Kern <robert.kern@gmail.com>
2019-10-25 12:19:33 -0400

    DOC: Expand comment.

--- qt_binder/qt/__init__.py
+++ qt_binder/qt/__init__.py
@@ -1,7 +1,10 @@
 from pyface.qt import is_qt4, is_qt5, qt_api  # noqa
 
 if qt_api == 'pyside2':
-    # Work around https://bugreports.qt.io/browse/PYSIDE-1093
+    # FIXME: Work around https://bugreports.qt.io/browse/PYSIDE-1093
+    # This is not a general solution, since most other Qt-using libraries might
+    # get run first. But this is sufficient to get the test suite to run for
+    # local development.
     from pyface.qt.QtGui import QApplication
 
     if QApplication.startingUp():
