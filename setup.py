7d99210690d2b13c2871dfaa3f641da66d48e0a0
Corran Webster <cwebster@enthought.com>
2022-08-15 09:45:34 +0100

    Release 0.4
    Version bump for release 0.4.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ QtBinder provides a way to bind Qt widgets to Traits models.
 
 setup(
     name='qt_binder',
-    version='0.3',
+    version='0.4',
     author='Enthought, Inc.',
     author_email='info@enthought.com',
     description='Traits bindings for Qt',
